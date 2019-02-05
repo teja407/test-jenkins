d8a36d0d6a393140487d603c72f4e788dde43c3a
teja407 <tejaatla123@gmail.com>
2019-02-05 22:54:07 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 for i in `cat inv`
 do
- ansible $i -m copy -a "src=/home/ec2-user/test-jenkins/copy.yml dest=/home/ec2-user"
+ ansible $i -m copy -a "src=/home/ec2-user/workspace/copy-job/copy.yml dest=/home/ec2-user"
 done
